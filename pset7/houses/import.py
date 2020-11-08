257b45c90993976c2ac32f8d0cb600acc1efac55
chxtio <chxtio@gmail.com>
2020-11-08 09:08:24 +0000

    Rewrote using sqlite3 library

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,33 +1,49 @@
 import csv
 import sys
-from cs50 import SQL
+#from cs50 import SQL
+import sqlite3
 
-def main():
-    loadData()
-
-def loadData():
-    # Check command line arguments
+def getCSV():
     if len(sys.argv) != 2:
         print('usage: import.py file.csv')
         sys.exit(1)
-    filename = sys.argv[1]
 
+    return sys.argv[1]
+
+def parseLines(ID, line):
+    name = line['name'].split(' ')
+    if len(name) == 3:
+        first, middle, last = name
+    else:
+        first, last = name
+        middle = None
+    student_info = [ID, first, middle, last]
+    student_info.append(line['house'])
+    student_info.append(line['birth'])
+
+    return student_info
+
+def importData(filename):
     # Connect to database
-    db = SQL('sqlite:///students.db')
+    #db = SQL('sqlite:///students.db')
+    con = sqlite3.connect('students.db')
+    db = con.cursor()
 
     # Import data from csv into database
     with open(filename, 'r') as characters:
         reader = csv.DictReader(characters)
         ID = 0
         for row in reader:
-            name = row['name'].split(' ')
-            if len(name) == 3:
-                first, middle, last = name
-            else:
-                first, last = name
-                middle = None
-            db.execute('INSERT INTO students (id, first, middle, last, house, birth) values(?, ?, ?, ?, ?, ?)', ID, first, middle, last, row['house'], row['birth'])
+            student = parseLines(ID, row)
+            db.execute('INSERT INTO students (id, first, middle, last, house, birth) values(?, ?, ?, ?, ?, ?)', student)
             ID += 1
+
+    con.commit()
+    con.close()
+
+def main():
+    filename = getCSV()
+    importData(filename)
 
 if __name__ == "__main__":
     main()
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,5 +1,6 @@
 import sys
-from cs50 import SQL
+#from cs50 import SQL
+import sqlite3
 
 def main():
     # Check command line arguments
@@ -9,17 +10,21 @@ def main():
     house = sys.argv[1]
 
     # Connect to database
-    db = SQL('sqlite:///students.db')
+    #db = SQL('sqlite:///students.db')
+    con = sqlite3.connect('students.db')
+    db = con.cursor()
 
     # Query database for all students in house
-    roster = db.execute('SELECT first, middle, last, birth FROM students WHERE house = (?) ORDER BY last, first', house)
+    roster = db.execute('SELECT first, middle, last, birth FROM students WHERE house = (?) ORDER BY last, first', (house,))
 
     # Print out students' full name and birth year
     for student in roster:
-        if student['middle'] == None:
-            print('{} {}, born {}'.format(student['first'], student['last'], student['birth']))
+        if student[1] == None:
+            print('{} {}, born {}'.format(student[0], student[2], student[3]))
         else:
-            print('{} {} {}, born {}'.format(student['first'], student['middle'], student['last'], student['birth']))
+            print('{} {} {}, born {}'.format(student[0], student[1], student[2], student[3]))
+
+    con.close()
 
 if __name__ == "__main__":
     main()
